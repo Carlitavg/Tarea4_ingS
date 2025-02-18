a03d63b8555464f5765a5e12c803e595407689de
Carla_Diana <carladianavgonzales@gmail.com>
2025-02-17 22:42:36 -0400

    feat: Saludar de acuerdo a la hora

--- src/saludador.js
+++ src/saludador.js
@@ -6,7 +6,19 @@ saludarButton.addEventListener("click", (event) => {
   event.preventDefault();
 
   const nombreSaludo = nombre.value;
+  let fechaActual = new Date();
+  let horaActual = fechaActual.getHours();
+  let saludoHora = "";
+
+  if (horaActual >= 5 && horaActual < 12) {
+    saludoHora = "Buenos días";
+  } else if (horaActual >= 12 && horaActual < 18) {
+    saludoHora = "Buenas tardes";
+  } else {
+    saludoHora = "Buenas noches";
+  }
 
   div.innerHTML = "<p>" + "Hola " + nombreSaludo + "</p>";
+  div.innerHTML = "<p>" + saludoHora +", "+ nombreSaludo + "</p>";
 });
 
